cab6d6c93acc0b8b6f8bbc2cee47d4848a59dc4f
Beibei <liubeibei789@gmail.com>
2019-04-22 15:42:54 -0700

    backtracking to find the route to get to a given "succeed" point at last row.
    Signed-off-by: Beibei <liubeibei789@gmail.com>

--- fuzzy.c
+++ fuzzy.c
@@ -2,10 +2,11 @@
 #include<stdlib.h>
 #include<string.h>
 
-#define MIN(a,b) (((a)<(b))?(a):(b))
-#define MAX(a,b) (((a)>(b))?(a):(b))
+#include "fuzzy.h"
 
-int readFile(char** buffer)
+#define K 1  //k Aprroximate Match(k diffs tolerated)
+
+int readFile(char** text)
 {
    FILE    *fp;
    long    numBytes;
@@ -21,13 +22,13 @@ int readFile(char** buffer)
    // reset the file position indicator to the beginning of the file
    fseek(fp, 0L, SEEK_SET);
 
-   *buffer = (char*)calloc(numBytes, sizeof(char));	
-   if(*buffer == NULL)
+   *text = (char*)calloc(numBytes, sizeof(char));	
+   if(*text == NULL)
       return 1;
  
-   fread(*buffer, sizeof(char), numBytes, fp);
+   fread(*text, sizeof(char), numBytes, fp);
    fclose(fp);
-   //printf("buffer:\n%s\n", *buffer);
+   //printf("text:\n%s\n", *text);
    printf("total:%ld bytes\n", numBytes);
    return 0;
 }
@@ -63,38 +64,38 @@ int findMin(int a, int b, int c)
 // function: kAprroximateMatch
 // fuzzy searching "pattern" in "text", 
 // with k differences tolerated.
-// m:pattern length, n:text length
+// m: length of pattern, n: length of text
 // return DP matrix
 //=====================================================
 
-int** kAprroximateMatch(char* pattern, char* text, int m, int n, int k)
+int** kAprroximateMatch(char* pattern, char* text, int m, int n)
 {
 	printf("enter kAprroximateMatch\n");
    printf("looking for %s\n", pattern);
 
 	// DP matrix memory allocation 
-	int** DP = calloc(m, sizeof(int*));
-   for (int i = 0; i < n; i++)
+	int** DP = calloc(m+1, sizeof(int*));
+   for (int i = 0; i < m+1; i++)
    {
-   	DP[i] = calloc(n, sizeof(int));
+   	DP[i] = calloc(n+1, sizeof(int));
    }
    
    // pattern is empty, 0 difference with text t1...tj
-   for (int j = 0; j < n; j++)
+   for (int j = 0; j < n+1; j++)
    {
    	DP[0][j] = 0;
    }
    // pattern p1...pi, i difference with empty text
-   for (int i = 0; i < m; i++)
+   for (int i = 0; i < m+1; i++)
    {
    	DP[i][0] = i;
    }
 
    // print before processing
    printf("original DP matrix:\n");
-   for (int i = 0; i < m; i++)
+   for (int i = 0; i < m+1; i++)
    {
-   	for (int j = 0; j < n; j++)
+   	for (int j = 0; j < n+1; j++)
    	{
    		printf("%d,",DP[i][j]);
    	}
@@ -103,12 +104,13 @@ int** kAprroximateMatch(char* pattern, char* text, int m, int n, int k)
 
    // fill the matrix
 
-   for (int j = 1; j < n; j++)
+   for (int j = 1; j < n+1; j++)
    {
-   	for (int i = 1; i < m; i++)
+   	for (int i = 1; i < m+1; i++)
    	{
-         if (pattern[i] == text[j])
+         if (pattern[i-1] == text[j-1])
          {
+            //printf("i=%d,pattern[i]=%c;j=%d,text[j]=%c\n",i-1,pattern[i-1],j-1,text[j-1]);
             DP[i][j] = findMin(DP[i-1][j-1], DP[i-1][j]+1, DP[i][j-1]+1);
             //printf("111:%d=%d,%d,%d\n",DP[i][j],DP[i-1][j-1], DP[i-1][j]+1, DP[i][j-1]+1);
          }
@@ -120,25 +122,29 @@ int** kAprroximateMatch(char* pattern, char* text, int m, int n, int k)
          //printf("pattern[i]=%c,text[j]=%c,DP[%d][%d]=%d\n", pattern[i],text[j],i,j,DP[i][j]);
 
    	}
-   	if (DP[m-1][j] <= k)
+      /*
+   	if (DP[m][j] <= k)
       {
          int len = strlen(pattern);
          printf("found...j=%d,", j);
          printf("len=%d,", len);
-         for(int p = 0; p < len; p++)
+         
+         for(int p = len; p > 0; p--)
          {
-            printf("%c,", text[j+p]);
+            printf("%c,", text[j-p+1]);
          }
          printf("\n");
+         
       	//return j;
       }
+      */
    }
 
    // print after filled
    printf("edited DP matrix:\n");
-   for (int i = 0; i < m; i++)
+   for (int i = 0; i < m+1; i++)
    {
-      for (int j = 0; j < n; j++)
+      for (int j = 0; j < n+1; j++)
       {
          printf("%d,",DP[i][j]);
       }
@@ -148,21 +154,120 @@ int** kAprroximateMatch(char* pattern, char* text, int m, int n, int k)
    return DP;
 }
 
-//int backtracking()
+//=====================================================
+// function: printing
+//=====================================================
+
+void printing(char* text, int start, int end)
+{
+   char *ptr = text;
+   ptr += start;
+   printf("%d-%d:", start, end);
+   for (int i = start; i <= end; i++)
+   {
+      printf("%c,", *ptr);
+   }
+   printf("\n");
+}
+
+//=====================================================
+// function: backtracking
+// find out the route to reach the "succeed point"
+// "succeed point":element less or equal than K
+//=====================================================
+
+#if 0
+int backtracking(char* text, int** DP, int m, int n, int k)
+{
+   printf("enter backtracking\n");
+   if (DP == NULL)
+   {
+      printf("DP NULL");
+      return -1;
+   }
+
+   Move moves[m+n+1]; //keep record of moves.max num of steps:m+n+1
+   int step = 0;  //can not know total num of step ahead, so record reversely only
+
+   // examine the last row for values less or equal to K
+   for (int j = 0; j < n+1; j++)
+   {
+      if (DP[m][j] <= k)
+      {
+         printf("found...idx=%d\n", j);
+         int jOriginal = j;
+         int i = m;
+         while(i >= 0 && j >= 0) //last(first before reverse) step reaches border
+         {
+            if (DP[i][j] == DP[i-1][j-1]) //DP[m][j] came diagonally, cur char matched
+            {
+               printf("the %d-th character of pattern matched", i);
+               moves[step++] = diag_same;
+            }
+            else
+            {
+               int minParent = findMin(DP[i-1][j-1],DP[i][j-1],DP[i-1][j]);
+               int diffPos;
+               
+               if (minParent+1 == DP[i-1][j-1]) // diagonal, cur char mismatched --> substitution
+               {
+                  moves[step++] = diag_diff;
+                  if (1 == k)
+                  {
+                     diffPos = j;
+                     printing(text, diffPos, jOriginal);
+                  }
+               } 
+               else if (minParent+1 == DP[i][j-1])  // horizontal --> insertion
+               {
+                  moves[step++] = hori;
+                  if (1 == k)
+                  {
+                     diffPos = j;
+                     printing(text, diffPos, jOriginal);
+                  }
+               }
+               else if (minParent+1 == DP[i-1][j]) // vertical --> deletion
+               {
+                  moves[step++] = vert;
+                  if (1 == k)
+                  {
+                     diffPos = j+1;
+                     printing(text, diffPos, jOriginal);
+                  }
+               } 
+               else
+               {
+                  printf("route calculation error\n");
+               }
+               
+            }
+
+         }
+      }
+   }
+
+
+   return 0;
+}
+#endif
 
 int main()
 {
-   char *buffer;
+   char *text;
    int **DP;
-   readFile(&buffer); //attention!!DO NOT pass "buffer"!remember &
-   char *str = "ware";
-   printf("buffer:\n%s\n", buffer);
-   ////findExact(buffer, str);
+   readFile(&text); //attention!!DO NOT pass "text"!remember &
+   char *pattern = "ware";
+   printf("text:\n%s\n", text);
+   ////findExact(text, pattern);
 
-   DP = kAprroximateMatch(str, buffer, 4, 7, 1);
+   int numRow = strlen(pattern);
+   int numCol = strlen(text);
 
+   DP = kAprroximateMatch(pattern, text, numRow, numCol);
+   //backtracking(text, DP, numRow, numCol, K);
 
-   free(buffer);
+   free(text);
 }
 
 
---
+++ fuzzy.h
@@ -0,0 +1,8 @@
+
+typedef enum move
+{
+	diag_same, //diagonal,same num
+	diag_diff, //diagonal,different num
+   hori, //horizontal
+   vert //vertical
+} Move;
